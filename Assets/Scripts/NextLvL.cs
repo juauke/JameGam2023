b60a0e5c093e0cb79080725fa0b0a5c6f4f891ee
enzo <enzo.mariant@ensiie.eu>
2023-10-29 03:45:13 +0100

    Grosse Update main
    Fusion de beaucoup de script,
    Ajout du stress ( quand on est au max on doit appuyer sur une touche)
    ajout des lights
    ajout du confiner de cam
    et peut etre d'autres trucs dont j'ai oublié l'existence :D bisou

--- Assets/Scripts/NextLvL.cs
+++ Assets/Scripts/NextLvL.cs
@@ -6,12 +6,14 @@ public class NextLvl : MonoBehaviour
 {
     [SerializeField] private Transform Checkpoint;
     [SerializeField] private Transform nextCheckpoint;
+    [SerializeField] private LevelTimer levelTimer;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.CompareTag("Player"))
         {
+            levelTimer.timeLeft = 0;
             var nextPos = nextCheckpoint.position;
             collision.gameObject.transform.position = nextPos;
             Checkpoint.position = nextPos;
--- Assets/Scripts/StressBar.cs
+++ Assets/Scripts/StressBar.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class StressBar : MonoBehaviour
 {
@@ -9,15 +14,13 @@ public class StressBar : MonoBehaviour
 
     [SerializeField] private int _stress=0;
     [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private List<Sprite> touches;
+    [SerializeField] private List<char> touchesChars;
+    [SerializeField] private Image spriteRenderer;
 
     private float timeLeftStress = 2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
+    [SerializeField] private float reactivite = 5f;
 
     public void AddStress(int moreStress)
     {
@@ -25,6 +28,46 @@ public class StressBar : MonoBehaviour
         if (newStress > _maxStress)
         {
             _stress=_maxStress;
+            StartCoroutine(MaxStress());
+            IEnumerator MaxStress()
+            {
+                var dead = false;
+                var entier = Random.Range(32, 128);
+                if(entier<=90 && entier >=65)
+                    entier+=91-65 ;
+                if (entier == 96) entier++;
+                var key = Convert.ToChar(entier).ToString();
+                Debug.Log(key);
+                var i = indexOfSprite(Convert.ToChar(entier));
+                if (!spriteRenderer.IsUnityNull()) 
+                {
+                    spriteRenderer.sprite = touches[i] ;
+                    Debug.Log(touches[i]);
+                    Debug.Log(i);
+                }
+                else
+                {
+                    Debug.Log("spriteRenderer is null");
+                }
+                var _startTime = Time.time;
+                while (Input.inputString != key && !dead)
+                {
+                    if(Input.anyKey)
+                        Debug.Log("SSSS "+ key + Input.inputString);
+                    if (Time.time - _startTime > reactivite)
+                    {
+                        dead = true;
+                        Debug.Log("you deadge");
+                    }
+                    yield return null;
+                }
+                if (!dead)
+                {
+                    _stress = 0;
+                    Debug.Log("you win");
+                }
+                UpdateBar();
+            }
         }
         else
         {
@@ -38,13 +81,34 @@ public class StressBar : MonoBehaviour
         _rectTransform.anchorMax=new Vector2((float) _stress/(float)_maxStress,1);
     }
 
+    private int indexOfSprite(char a)
+    {
+        for (var i= 0; i < touchesChars.Count; i++)
+        {
+            if (touchesChars[i] == a)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     void Update()
     {
         timeLeftStress -= Time.deltaTime;
         if (timeLeftStress <= 0)
         {
-            AddStress(1);
+            if (_stress < 100)
+            {
+                
+                AddStress(50);
             timeLeftStress = Random.Range(1f, 3f);
+            }
+            else
+            {
+                AddStress(50);
+                timeLeftStress = Random.Range(5f, 10f);
+            }
         }
     }
 }
